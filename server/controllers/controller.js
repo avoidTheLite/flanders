a5d3186691c9e57eb1ff6b205876de8a5d3d0df1
avoidTheLite <metalsupremacist@gmail.com>
2023-06-25 21:05:07 -0400

    fixed route for /route1/:ID

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,5 +1,5 @@
-export function controllerFunction1 = (req, res) => {
-
+export function controllerFunction1(req, res) {
+//we pretend this retrieves some data
     const controllerData1 ={
 
     ID: req.params['ID'],
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 const router = Router();
-import {controllerFunction1} from '../controllers/controller';
+import {controllerFunction1} from '../controllers/controller.js';
 
 router.use((req, res, next) => {
     //pre-route prep
     next()
 })
 
-router.get('/route1:ID', (req,res)=> {
+router.get('/route1/:ID', (req,res)=> {
     
     const responseJson = controllerFunction1(req,res)
            

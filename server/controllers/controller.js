340061726f1da72903d541cc35dd67e3dfbf7f19
avoidTheLite <metalsupremacist@gmail.com>
2023-06-25 20:23:09 -0400

    added controller.js and updated routes 2

---
+++ server/controllers/controller.js
@@ -0,0 +1,14 @@
+controllerFunction1 = (req, res) => {
+
+    const controllerData1 ={
+
+    ID: req.params['ID'],
+    name: "Robert Pulson",
+    type: 'example',
+    url: "https://en.wikipedia.org/wiki/Special:Random"
+
+    };
+return controllerData1
+};
+
+export {controllerFunction1};

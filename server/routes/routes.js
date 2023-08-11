4fe03e843f1d627e320ded01422b2e186cd33ee9
avoidTheLite <metalsupremacist@gmail.com>
2023-08-11 17:59:28 -0400

    pulled in template version of indexed controllers, a DB model, and services for calling external APIs and formatting a response. This came from flanders-chat

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,22 +1,53 @@
 import { Router } from 'express';
 const router = Router();
-import {controllerFunction1} from '../controllers/controller.js';
+import bodyParser from "body-parser";
+import controllers from '../controllers/index.js';
+import { successFormatter } from '../services/responseAPI.js';
+import { errorFormatter } from '../services/responseAPI.js';
+
+const jsonParser = bodyParser.json();
 
 router.use((req, res, next) => {
     //pre-route prep
     next()
 })
 
-router.get('/route1/:ID', (req,res)=> {
-    
+router.get('/getroute1/:inputID', (req,res)=> {
+    const inputID = req.params['inputID'];
+
+    try {
+        const results = controllers.controllerFunction1(req,res, inputID)
+        const message = `Successfully retrieved with inputID = ${inputID}`
+        res.send(successFormatter(message, results, 200))
     const responseJson = controllerFunction1(req,res)
+    } catch (error) {
+        res.send(errorFormatter(error.message, 500))
+    }
            
-    res.send(responseJson);
 })
 
-router.get('/route2', (req,res) => {
-    //do something with a static route 
-    res.send('/route2 doesnt have a controller, but you can call it anyways...');
+router.get('/getroute2', (req,res) => {
+
+    try {
+        const results = controllers.controllerFunction2(req,res)
+        const message = `Successfully retrieved`
+        res.send(successFormatter(message, results, 200))
+    const responseJson = controllerFunction1(req,res)
+    } catch (error) {
+        res.send(errorFormatter(error.message, 500))
+    }
+
+})
+
+router.post('/postroute1', jsonParser, async (req,res,) => {
+    
+    try {
+        const results = controllers.controllerFunction3(req,res)
+        const message = `Successfully retrieved`
+        res.send(successFormatter(message, results, 200))
+    } catch (error) {
+        
+    }
 })
 
 export default router

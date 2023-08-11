dcbfcff533fd3a1bd6c458bd88bf946819195b0e
avoidTheLite <metalsupremacist@gmail.com>
2023-08-11 17:59:45 -0400

    pulled in template version of indexed controllers, a DB model, and services for calling external APIs and formatting a response. This came from flanders-chat

---
+++ server/controllers/index.js
@@ -0,0 +1,7 @@
+import { controllerFunction1 } from "./lib/controllerFunction1"
+import { controllerFunction2 } from "./lib/controllerFunction2";
+
+export default {
+    controllerFunction1,
+    controllerFunction2
+};
